a1e14ddfd138689db11b2c14c4a92cf060774a09
jos3lo89 <ganbaru743@gmail.com>
2026-01-31 16:05:15 -0500

    configuration main ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,52 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 5000);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'debug', 'log', 'fatal', 'verbose'],
+  });
+
+  app.use(cookieParser());
+
+  const logger = new Logger('bootstrap');
+  const configService = app.get(ConfigService);
+
+  const corsOrigins = configService
+    .get<string>('CORS_ORIGINS', '')
+    .split(',')
+    .map((origin) => origin.trim());
+
+  const isProd = configService.get('NODE_ENV') === 'production';
+
+  app.enableCors({
+    origin: isProd ? corsOrigins : true,
+    credentials: true,
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const apiPrefix = configService.get<string>('API_PREFIX', 'api/v1');
+  app.setGlobalPrefix(apiPrefix, {
+    exclude: ['health'],
+  });
+
+  app.enableShutdownHooks();
+
+  const port = configService.get<number>('PORT', 5000);
+  const nodeEnv = configService.get<string>('NODE_ENV');
+  await app.listen(port);
+
+  logger.log(
+    `🚀 Application is running on: ${await app.getUrl()}, NODE_ENV: ${nodeEnv},`,
+  );
 }
 bootstrap();
