d0ca2daa8f600cba44db16877b8e589f61bd049d
jos3lo89 <ganbaru743@gmail.com>
2026-02-01 16:11:20 -0500

    feat: category search

--- src/modules/categories/categories.controller.ts
+++ src/modules/categories/categories.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { CreatedDto } from './dto/created.dto';
 import { CategoriesService } from './categories.service';
 import { Auth } from '@/common/decorators/auth.decorator';
 import { UserRole } from '@/generated/prisma/enums';
+import { FindCategoryQueryDto } from './dto/find-category-query.dto';
 
 @Controller('categories')
 export class CategoriesController {
@@ -15,6 +16,12 @@ export class CategoriesController {
   }
 
   @Get()
+  @Auth()
+  getCategories(@Query() query: FindCategoryQueryDto) {
+    return this.categorieService.getCategories(query);
+  }
+
+  @Get('all')
   @Auth()
   getAllCategories() {
     return this.categorieService.getAllCategories();
--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -1,6 +1,8 @@
 import { PrismaService } from '@/core/prisma/prisma.service';
 import { ConflictException, Injectable } from '@nestjs/common';
 import { CreatedDto } from './dto/created.dto';
+import { FindCategoryQueryDto } from './dto/find-category-query.dto';
+import { Prisma } from '@/generated/prisma/client';
 
 @Injectable()
 export class CategoriesService {
@@ -22,8 +24,57 @@ export class CategoriesService {
     return categoryNew;
   }
 
+  async getCategories(query: FindCategoryQueryDto) {
+    const { page = 1, limit = 5, search } = query;
+    const skip = (page - 1) * limit;
+
+    const where: Prisma.CategoryWhereInput = search
+      ? {
+          OR: [
+            { name: { contains: search, mode: 'insensitive' } },
+            { slug: { contains: search, mode: 'insensitive' } },
+          ],
+        }
+      : {};
+
+    const [total, categories] = await this.prisma.$transaction([
+      this.prisma.category.count({ where }),
+      this.prisma.category.findMany({
+        where,
+        skip,
+        take: limit,
+        orderBy: { createdAt: 'desc' },
+        include: {
+          _count: {
+            select: {
+              books: true,
+            },
+          },
+        },
+      }),
+    ]);
+
+    const lastPage = Math.ceil(total / limit);
+    const next = page < lastPage ? page + 1 : null;
+    const prev = page > 1 ? page - 1 : null;
+
+    return {
+      data: categories,
+      meta: {
+        total,
+        page,
+        lastPage,
+        hasNext: page < lastPage,
+        hasPrev: page > 1,
+        nextPage: next,
+        prevPage: prev,
+      },
+    };
+  }
+
   async getAllCategories() {
     const categories = await this.prisma.category.findMany({
+      orderBy: { createdAt: 'desc' },
       include: {
         _count: {
           select: {
@@ -32,7 +83,6 @@ export class CategoriesService {
         },
       },
     });
-
     return categories;
   }
 }
